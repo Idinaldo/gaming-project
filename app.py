d41652b0c61c01f93e832fd81f5326ff435849bb
Werisson  Félix <151643029+WerissonFelix@users.noreply.github.com>
2024-10-11 11:09:42 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+from entidades import *
 
 app = Flask(__name__)
 
@@ -10,24 +11,36 @@ def index():
 
 @app.route('/criar', methods=['POST']) 
 def create():
-    nome = request.form['nome']
-    games.append(nome)
+    current_game = Game()
+    current_game.setNome(request.form['nome'])
+    current_game.setDev(request.form['desenvolvedora'])
+    current_game.setDesc(request.form['descricao'])
+    games.append(current_game)
     return redirect('/')
 
 @app.route('/alterar', methods=['POST']) # Rota /alterar
 def update():
     old_name = request.form['old_name']
     new_name = request.form['new_name']
-    if old_name in games:
-        index = games.index(old_name)
-        games[index] = new_name
-    return redirect('/')
+    #old_name_desen = request.form['nome antigo da desenvolvedora']
+    new_name_dev = request.form['Novo nome da desenvolvedora']
+    for g in games:
+        if g.getNome() == old_name:
+            g.setNome(new_name)
+            g.setDev(new_name_dev)
+            return redirect('/')
+    else:
+        return "Jogo não encontrado"
+    #elif old_name_desen in games:
 
 @app.route('/apagar', methods=['POST']) # Rota /apagar
 def delete():
     nome = request.form['nome']
-    if nome in games:
-        games.remove(nome)
-    return redirect('/')
+    for g in games:
+        if g.getNome() == nome:
+            games.remove(g)
+            return redirect('/')
+    else:
+        return "jogo não encontrado"
 if __name__ == '__main__':
     app.run(debug=True)
